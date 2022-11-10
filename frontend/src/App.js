c1dc72ae2b9d4bdbef3ba2dce6e6fe488e96e351
JaishyamRB <jaishyam1998@gmail.com>
2022-11-10 01:24:14 -0500

    Made fronted pages, routes and header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import pages
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+// import components
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <>
+    <Router>
+    <div className='container'>
+    <Header/>
+    <Routes>
+    
+      <Route path='/' element = {<Dashboard/>}/>
+      <Route path='/register' element = {<Register/>}/>
+      <Route path='/login' element = {<Login/>}/>
+    
+    </Routes>
     </div>
+    </Router>
+  </>
+    
   );
 }
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='container'>dashboard</div>
+  )
+}
+
+export default Dashboard
